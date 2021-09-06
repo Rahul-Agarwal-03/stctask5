70168c0d1bf52c930bb94b16a22228cb3d9a1fd0
Rahul-Agarwal-03 <rahul03agarwal02@gmail.com>
2021-09-06 22:21:40 +0530

    almost done

---
+++ home.js
@@ -0,0 +1,27 @@
+import React from "react"
+import {Link}  from "react-router-dom"
+import ticket from "./ticket.png"
+
+function Home(){
+    return(
+        <div>
+            {/* <Link to="/">
+                <button>Home</button>
+            </Link> */}
+            <Link to="/SignInEmployee">
+             <button className="ms-5 btn btn-warning">Sign in as Employee</button>
+            </Link>
+            <Link to="/SignInAdmin">
+             <button className="ms-5 btn btn-warning">Sign in as Admin</button>
+            </Link>
+            <Link to="/SignUpEmployee">
+             <button className="ms-5 btn btn-warning">Sign up as Employee</button>
+            </Link>
+            <Link to="/SignUpAdmin">
+             <button className="ms-5 btn btn-warning">Sign up as Admin</button>
+            </Link>
+            <img src={ticket} alt="..."></img>
+        </div>
+    )
+}
+export default Home
